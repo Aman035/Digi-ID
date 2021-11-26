340f41aef8086cda060f89e25489cb92aa4f1dda
Aman <guptaaman200115@gmail.com>
2021-11-26 13:23:04 +0530

    Frontend contract update and refresh rate change

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,6 @@
-export const REFRESH_RATE = 10000;
+export const REFRESH_RATE = 6000;
 export const ALERT_DELAY = 6000;
-export const CONTRACT_ADDRESS = '0x384D0D5eD8A094511b804dcde855f6AAe3416476';
+export const CONTRACT_ADDRESS = '0xc5a422571d21EeCe071Ac2f61dc6c9FcC61137A6';
 export const delay = async() => (
     // return await for better async stack trace support in case of errors.
     await new Promise(resolve => setTimeout(resolve, REFRESH_RATE))
